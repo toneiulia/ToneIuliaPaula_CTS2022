a5e4a2c08cae8570ece9b08325b1cbea8d810342
IULIAPAULA TONE <TONEIULIA19@stud.ase.ro>
2022-05-02 21:36:07 +0300

    Seminarul 9 (2.05) - Template

---
+++ Seminar9_2.05_Template/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import clase.MijlocTransportPeSine;
+import clase.Tramvai;
+
+public class Main {
+    public static void main(String[] args) {
+        MijlocTransportPeSine tramvai = new Tramvai();
+        tramvai.parcurgereTraseu();
+        System.out.println();
+        tramvai.parcurgereTraseuInvers();
+    }
+}
